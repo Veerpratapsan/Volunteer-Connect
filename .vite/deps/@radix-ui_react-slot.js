687eb492756f07ecf3ba4fa72341cd1e12e1ef2b
Veer Pratap Singh <veerpratapsan@gmail.com>
2026-04-28 20:03:10 +0530

    added frontend

---
+++ .vite/deps/@radix-ui_react-slot.js
@@ -0,0 +1,13 @@
+import {
+  Root,
+  Slot,
+  Slottable
+} from "./chunk-R2MP5X5J.js";
+import "./chunk-RKK2FJ3C.js";
+import "./chunk-QTVD6AVW.js";
+import "./chunk-PR4QN5HX.js";
+export {
+  Root,
+  Slot,
+  Slottable
+};
